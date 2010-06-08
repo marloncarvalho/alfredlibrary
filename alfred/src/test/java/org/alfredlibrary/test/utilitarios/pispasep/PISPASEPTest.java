3c988cd732f3c80b471da06d3cef7ce1007f474d
rmfagundes <rmfagundes@482f4288-4fb1-11de-a7b9-cdf4705868f5>
2010-06-08 18:28:23 +0000

    Conclusão da implementação e dos testes de utilitário, validador e formatador de PIS/PASEP.

---
+++ alfred/src/test/java/org/alfredlibrary/test/utilitarios/pispasep/PISPASEPTest.java
@@ -0,0 +1,44 @@
+/*
+ *  This file is part of Alfred Library.
+ *
+ *  Alfred Library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Alfred Library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Alfred Library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfredlibrary.test.utilitarios.pispasep;
+
+import junit.framework.Assert;
+
+import org.alfredlibrary.utilitarios.pispasep.PISPASEP;
+import org.junit.Test;
+
+/**
+ * Classe de Teste para o utilitário de PIS/PASEP.
+ * 
+ * @author Rodrigo Moreira Fagundes
+ * @since 08/06/2010
+ */
+public class PISPASEPTest {
+
+	@Test
+	public void testGerarDigitoVerificador() {
+		String digito = PISPASEP.gerarDigitoVerificador("1111111111");
+		Assert.assertEquals("4", digito);
+	}
+	
+	@Test
+	public void testGerarPISPASEP() {
+		String pisPasep = PISPASEP.gerar();
+		Assert.assertTrue(org.alfredlibrary.validadores.PISPASEP.isValido(pisPasep));
+	}
+
+}
---
+++ alfred/src/test/java/org/alfredlibrary/test/validadores/PISPASEPTest.java
@@ -0,0 +1,47 @@
+/*
+ *  This file is part of Alfred Library.
+ *
+ *  Alfred Library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Alfred Library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Alfred Library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfredlibrary.test.validadores;
+
+import junit.framework.Assert;
+
+import org.alfredlibrary.validadores.PISPASEP;
+import org.junit.Test;
+
+/**
+ * Classe de Teste para o Validador de PIS/PASEP.
+ * 
+ * @author Rodrigo Moreira Fagundes
+ * @since 08/06/2010
+ */
+public class PISPASEPTest {
+
+
+	@Test
+	public void testarValidarPISPASEPValido() {
+		if ( !PISPASEP.isValido("111.11111.11-4") ) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testarValidarPISPASEPInvalido() {
+		if ( PISPASEP.isValido("111.11111.11-3") ) {
+			Assert.fail();
+		}
+	}
+	
+}
