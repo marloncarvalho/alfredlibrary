dc228001d626f4df8c56dbd29b2647d9e56c8c4c
marloncarvalho <marloncarvalho@482f4288-4fb1-11de-a7b9-cdf4705868f5>
2010-08-05 21:32:47 +0000

    Corrigindo código conforme sugerido pelo PMD. Esquecido de colocar "final" nos campos de constantes. Caracteres não UTF-8 removidos.

--- alfred/src/main/java/org/alfredlibrary/utilitarios/idiomas/Babelfish.java
+++ alfred/src/main/java/org/alfredlibrary/utilitarios/idiomas/Babelfish.java
@@ -38,50 +38,49 @@ final public class Babelfish {
 	//public static String CHINESTRADICIONAL_PARA_CHINESSIMP= "zt_zh";
 	//public static String INGLES_PARA_CHINESSIMPL= "en_zh";
 	//public static String INGLES_PARA_CHINESTRADICIONAL = "en_zt";
-	public static String INGLES_PARA_HOLANDES = "en_nl";
-	public static String INGLES_PARA_FRANCES = "en_fr";
-	public static String INGLES_PARA_ALEMAO = "en_de";
+	public final static String INGLES_PARA_HOLANDES = "en_nl";
+	public final static String INGLES_PARA_FRANCES = "en_fr";
+	public final static String INGLES_PARA_ALEMAO = "en_de";
 	//public static String INGLES_PARA_GREGO = "en_el";
-	public static String INGLES_PARA_ITALIANO = "en_it";
+	public final static String INGLES_PARA_ITALIANO = "en_it";
 	//public static String INGLES_PARA_JAPONES = "en_ja";
 	//public static String INGLES_PARA_COREANO = "en_ko";
-	public static String INGLES_PARA_PORTUGUES = "en_pt";
+	public final static String INGLES_PARA_PORTUGUES = "en_pt";
 	//public static String INGLES_PARA_RUSSO = "en_ru";
-	public static String INGLES_PARA_ESPANHOL = "en_es";
-	public static String HOLANDES_PARA_INGLES = "nl_en";
-	public static String HOLANDES_PARA_FRANCES = "nl_fr";
-	public static String FRANCES_PARA_HOLANDES = "fr_nl";
-	public static String FRANCES_PARA_INGLES = "fr_en";
-	public static String FRANCES_PARA_ALEMAO = "fr_de";
+	public final static String INGLES_PARA_ESPANHOL = "en_es";
+	public final static String HOLANDES_PARA_INGLES = "nl_en";
+	public final static String HOLANDES_PARA_FRANCES = "nl_fr";
+	public final static String FRANCES_PARA_HOLANDES = "fr_nl";
+	public final static String FRANCES_PARA_INGLES = "fr_en";
+	public final static String FRANCES_PARA_ALEMAO = "fr_de";
 	//public static String FRANCES_PARA_GREGO = "fr_el";
-	public static String FRANCES_PARA_ITALIANO = "fr_it";
-	public static String FRANCES_PARA_PORTUGUES = "fr_pt";
-	public static String FRANCES_PARA_ESPANHOL = "fr_es";
-	public static String ALEMAO_PARA_INGLES = "de_en";
-	public static String ALEMAO_PARA_FRANCES = "de_fr";
+	public final static String FRANCES_PARA_ITALIANO = "fr_it";
+	public final static String FRANCES_PARA_PORTUGUES = "fr_pt";
+	public final static String FRANCES_PARA_ESPANHOL = "fr_es";
+	public final static String ALEMAO_PARA_INGLES = "de_en";
+	public final static String ALEMAO_PARA_FRANCES = "de_fr";
 	//public static String GREGO_PARA_INGLES = "el_en";
 	//public static String GREGO_PARA_FRANCES = "el_fr";
-	public static String ITALIANO_PARA_INGLES = "it_en";
-	public static String ITALIANO_PARA_FRANCES = "it_fr";
+	public final static String ITALIANO_PARA_INGLES = "it_en";
+	public final static String ITALIANO_PARA_FRANCES = "it_fr";
 	//public static String JAPONES_PARA_INGLES = "ja_en";
 	//public static String COREANO_PARA_INGLES = "ko_en";
-	public static String PORTUGUES_PARA_INGLES = "pt_en";
-	public static String PORTUGUES_PARA_FRANCES = "pt_fr";
+	public final static String PORTUGUES_PARA_INGLES = "pt_en";
+	public final static String PORTUGUES_PARA_FRANCES = "pt_fr";
 	//public static String RUSSO_PARA_INGLES = "ru_en";
-	public static String ESPANHOL_PARA_INGLES = "es_en";
-	public static String ESPANHOL_PARA_FRANCES = "es_fr";
+	public final static String ESPANHOL_PARA_INGLES = "es_en";
+	public final static String ESPANHOL_PARA_FRANCES = "es_fr";
 		
 	private Babelfish() { }
 
 	/**
-	 * Realizar a tradu��o de um idioma para outro.
+	 * Realizar a tradução de um idioma para outro.
 	 * 
-	 * @param idioma Tradu��o que ser� realizada.
+	 * @param idioma Tradução que será realizada.
 	 * @param palavra Palavra ou frase a ser traduzida.
 	 * @return Palavra ou frase traduzida.
 	 */
 	public static String traduzir(String idioma, String palavra) {
-		// Montar os par�metros.
 		Map<String, String> parametros = new HashMap<String, String>();
 		parametros.put("lp", idioma);
 		parametros.put("ei","UTF-8");
@@ -91,21 +90,18 @@ final public class Babelfish {
 		parametros.put("tt", "urltext");
 		parametros.put("trtext", palavra);
 
-		// Realizar a requisi��o.
 		String conteudo = new String(WorldWideWeb.obterConteudoSite("http://br.babelfish.yahoo.com/translate_txt", parametros));
-		
-		// Usar express�o regular para achar o pre�o.
+
 		Pattern padrao = Pattern.compile("<div id=\"result\"><div style=\"padding:0.6em;\">[\\d\\s\\w-]+</div></div>");  
 		Matcher pesquisa = padrao.matcher(conteudo);
 
-		// Deve encontrar apenas um.
 		String traducao = null;
 		while(pesquisa.find()) {
 			traducao = pesquisa.group();
 		}
 		if ( traducao != null )
 			return traducao.replaceAll("<div id=\"result\"><div style=\"padding:0.6em;\">","").replaceAll("</div>","");
-		throw new AlfredException("Tradu��o n�o encontrada.");
+		throw new AlfredException("Tradução não encontrada.");
 	}
 
 }
